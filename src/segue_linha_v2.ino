7fafabf5555910806c175659265fb606398d0895
agonilha <henriqueagonilha@gmail.com>
2026-09-22 15:28:04 -0300

    Versão da primeira corrida

--- src/segue_linha_v2.ino
+++ src/segue_linha_v2.ino
@@ -13,13 +13,10 @@ const int SENSOR_DIREITO = 2; // Pino do sensor TCRT direito
 const int SENSOR_CENTRO = 3; // Pino do sensor TCRT central
 const int SENSOR_ESQUERDO = 4; // Pino do sensor TCRT esquerdo
 
-<<<<<<< HEAD
 int leituraDireito;
 int leituraCentro;
 int leituraEsquerdo;
 
-=======
->>>>>>> 1a87191a3af37c85ca1802db13b6a5a88d9c0153
 void setup() {
     // Configura os pinos dos motores como saída
     pinMode(MOTOR_A_DIR_1, OUTPUT);
@@ -40,11 +37,7 @@ void loop() {
     seguidor(); // Chama a função seguidor de linha
 }
 
-<<<<<<< HEAD
 void leitura() {
-=======
-leitura() {
->>>>>>> 1a87191a3af37c85ca1802db13b6a5a88d9c0153
     leituraDireito = digitalRead(SENSOR_DIREITO); // Leitura do sensor direito
     leituraCentro = digitalRead(SENSOR_CENTRO); // Leitura do sensor central
     leituraEsquerdo = digitalRead(SENSOR_ESQUERDO); // Leitura do sensor esquerdo
@@ -52,54 +45,29 @@ leitura() {
 
 void seguidor() {
     // Linha no meio ou em todos os sensores
-<<<<<<< HEAD
-    if (((leituraDireito == 0) && (leituraCentro == 1) && (leituraEsquerdo == 0)) ||
-        ((leituraDireito == 1) && (leituraCentro == 1) && (leituraEsquerdo == 1)) ||
-        ((leituraDireito == 0) && (leituraCentro == 1) && (leituraEsquerdo == 1)) ||
-        ((leituraDireito == 1) && (leituraCentro == 1) && (leituraEsquerdo == 0))) {
-=======
-    if (((leituraDireito == 0) && (leituraCentro == 1) && (leituraEsquerdo == 0)) || ((leituraDireito == 1) && (leituraCentro == 1) && (leituraEsquerdo == 1))) {
->>>>>>> 1a87191a3af37c85ca1802db13b6a5a88d9c0153
-        frente();
+    if (leituraCentro == 1) {
+        frente(180);
+    } else if (leituraDireito == 1 && leituraEsquerdo == 0) {
+        direita(180);
+    } else if (leituraEsquerdo == 1 && leituraDireito == 0) {
+        esquerda(180);
+    } else if (leituraDireito == 1 && leituraEsquerdo == 1) {
+        direita(180);
     }
-
-    // Linha na direita ou no centro e direita
-<<<<<<< HEAD
-    if (((leituraDireito == 1) && (leituraCentro == 0) && (leituraEsquerdo == 0))) {
-=======
-    if (((leituraDireito == 1) && (leituraCentro == 0) && (leituraEsquerdo == 0)) || ((leituraDireito == 1) && (leituraCentro == 1) && (leituraEsquerdo == 0))) {
->>>>>>> 1a87191a3af37c85ca1802db13b6a5a88d9c0153
-        direita();
-    }
-
-    // Linha na esquerda ou no centro e esquerda
-<<<<<<< HEAD
-    if (((leituraDireito == 0) && (leituraCentro == 0) && (leituraEsquerdo == 1))) {
-=======
-    if (((leituraDireito == 0) && (leituraCentro == 0) && (leituraEsquerdo == 1)) || ((leituraDireito == 0) && (leituraCentro == 1) && (leituraEsquerdo == 1))) {
->>>>>>> 1a87191a3af37c85ca1802db13b6a5a88d9c0153
-        esquerda();
-    }
-
-    // Se detectar apenas os sensores externos, ele vai para a direita
-    if ((leituraDireito == 1) && (leituraCentro == 0) && (leituraEsquerdo == 1)) {
-        direita();
-    }
-<<<<<<< HEAD
 }
 
-void frente() {
+void frente(int velocidade) {
     // Motor A para frente
     digitalWrite(MOTOR_A_DIR_1, HIGH);
     digitalWrite(MOTOR_A_DIR_2, LOW);
-    analogWrite(MOTOR_A_PWM, 255);
+    analogWrite(MOTOR_A_PWM, velocidade);
 
     // Motor B para frente
     digitalWrite(MOTOR_B_DIR_1, HIGH);
     digitalWrite(MOTOR_B_DIR_2, LOW);
-    analogWrite(MOTOR_B_PWM, 255);
+    analogWrite(MOTOR_B_PWM, velocidade);
 }
-void direita() {
+void direita(int velocidade) {
     // Motor A parado
     digitalWrite(MOTOR_A_DIR_1, LOW);
     digitalWrite(MOTOR_A_DIR_2, LOW);
@@ -108,19 +76,17 @@ void direita() {
     // Motor B para frente
     digitalWrite(MOTOR_B_DIR_1, HIGH);
     digitalWrite(MOTOR_B_DIR_2, LOW);
-    analogWrite(MOTOR_B_PWM, 255);
+    analogWrite(MOTOR_B_PWM, velocidade);
 
 }
-void esquerda() {
+void esquerda(int velocidade) {
     // Motor A para frente
     digitalWrite(MOTOR_A_DIR_1, HIGH);
     digitalWrite(MOTOR_A_DIR_2, LOW);
-    analogWrite(MOTOR_A_PWM, 255);
+    analogWrite(MOTOR_A_PWM, velocidade);
     
     // Motor B parado
     digitalWrite(MOTOR_B_DIR_1, LOW);
     digitalWrite(MOTOR_B_DIR_2, LOW);
     analogWrite(MOTOR_B_PWM, 0);
-=======
->>>>>>> 1a87191a3af37c85ca1802db13b6a5a88d9c0153
 }
