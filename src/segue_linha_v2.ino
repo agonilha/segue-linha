317536e90755b31632ed2f6e9b62389aaf043bb5
agonilha <henriqueagonilha@gmail.com>
2026-09-23 08:20:27 -0300

    Revert "Versão para a segunda corrida"
    This reverts commit 255b651b4af707badd6a1782abf74f02103ffb09.

--- src/segue_linha_v2.ino
+++ src/segue_linha_v2.ino
@@ -46,6 +46,7 @@ void leitura() {
 void seguidor() {
     // Linha no meio ou em todos os sensores
     if (leituraCentro == 1) {
+<<<<<<< HEAD
         frente(130);
     } else if (leituraDireito == 1) {
         direita(130);
@@ -53,6 +54,15 @@ void seguidor() {
         esquerda(130);
     } else if (leituraDireito == 1 && leituraCentro == 0 && leituraEsquerdo == 1) {
         direita(130);
+=======
+        frente(180);
+    } else if (leituraDireito == 1 && leituraEsquerdo == 0) {
+        direita(180);
+    } else if (leituraEsquerdo == 1 && leituraDireito == 0) {
+        esquerda(180);
+    } else if (leituraDireito == 1 && leituraEsquerdo == 1) {
+        direita(180);
+>>>>>>> parent of 255b651 (Versão para a segunda corrida)
     }
 }
 
